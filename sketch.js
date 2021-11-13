1ea7bab3235a49fa079310cd5a44ac53a281e926
Nimit17062008 <92149487+Nimit17062008@users.noreply.github.com>
2021-11-13 20:37:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+var ball = {
+  x : 20 ,
+  y : 25 ,
+  r : 20 ,
+  xspeed : 0 ,
+  yspeed : 0 ,
+  colour : ['black','skyblue','green']
+}
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  fill (ball.colour[1])
+  circle (ball.x,ball.y,ball.r)
+  ball.xspeed = 1
+  ball.x = ball.x + ball.xspeed
 }
